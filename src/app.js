f2e6f67e08968f73c680141b977304df86f5bcb0
igor-ribeir0 <igor_ribeirosantos@outlook.com>
2023-02-26 20:24:59 -0300

    feat:POST/signUp AND refector:POST/signIn

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ import { db } from "./config/dataBase.js";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from 'uuid';
 
+/*{
+  "name": "Naruto",
+  "email": "naruto@gg.com",
+  "password": "narutinho",
+  "confirmPassword": "narutinho"
+}
+*/
+
 dotenv.config();
 
 const app = express();
@@ -14,6 +22,15 @@ const signInSchema = joi.object(
     {
         email: joi.string().email().required(),
         password: joi.string().required()
+    }
+);
+
+const signUpSchema = joi.object(
+    {
+        name: joi.string().max(60).required(),
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+        confirmPassword: joi.string().valid(joi.ref('password')).required()
     }
 );
 
@@ -39,9 +56,13 @@ app.post("/signIn", async(req, res) => {
             [email]
         );
 
-        const comparePassword = bcrypt.compareSync(password, user.rows.password);
+        if(user.rowCount !== 0){
+            const comparePassword = bcrypt.compareSync(password, user.rows[0].password);
 
-        if(user.rowCount === 0 || comparePassword === false){
+            if(comparePassword === false) return res.sendStatus(401);
+        }
+
+        if(user.rowCount === 0){
             return res.sendStatus(401);
         }
 
@@ -53,10 +74,47 @@ app.post("/signIn", async(req, res) => {
                 INSERT INTO sessions ("userId", token)
                 VALUES ($1, $2)
             `,
-            [user.rows.id, token]
+            [user.rows[0].id, token]
         );
 
         res.status(200).send(userToken);
+    }
+    catch(error){
+        res.status(500).send(error.message);
+    }
+});
+
+app.post("/signUp", async(req, res) => {
+    const { name, email, password, confirmPassword } = req.body;
+    const registerTest = { name, email, password, confirmPassword };
+    const validation = signUpSchema.validate(registerTest, { abortEarly: false });
+
+    if(validation.error){
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try{
+        const user = await db.query(
+            `
+                SELECT * from users WHERE email = $1
+            `,
+            [email]
+        );
+
+        if(user.rowCount !== 0) return res.sendStatus(409);
+
+        const hash = bcrypt.hashSync(password, 10);
+
+        await db.query(
+            `
+                INSERT INTO users (name, email, password)
+                VALUES ($1, $2, $3)
+            `,
+            [name, email, hash]
+        );
+
+        res.sendStatus(201);
     }
     catch(error){
         res.status(500).send(error.message);
